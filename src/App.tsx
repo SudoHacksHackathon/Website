2bffb19b55f56a6ea23a1750a31e1c460e33f0a5
Ayush Garg <ayushgargemail@gmail.com>
2022-07-19 19:41:19 -0700

    added forward/back buttons, made 100% responsive!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import EndScreen from "./screens/EndScreen";
 import getOSData from "./util/getOSData";
 
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 
 const App = () => {
   const ip = useIP();
@@ -16,22 +17,28 @@ const App = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-slate-900 h-full w-full flex justify-center place-items-center selection:bg-slate-200 selection:text-black">
-      <div className="rounded-xl w-3/4 h-[80vh] bg-black font-mono flex flex-col">
+    <div className="bg-slate-900 h-full w-full flex justify-center place-items-center selection:bg-slate-200 selection:text-black p-3">
+      <div className="rounded-xl w-[min(100%,_75rem)] h-[min(100%,_42.1875rem)] bg-black font-mono flex flex-col">
         <div className="rounded-t-xl w-full h-7 bg-white flex place-items-center justify-center">
           <div className="flex-1 flex justify-start align-items-center pl-5">
-            {"<"}
+            <button onClick={() => navigate(-1)}>
+              <MdArrowBackIosNew />
+            </button>
           </div>
+
           <div className="flex-1 flex justify-center align-items-center">
             <p className="md:text-base text-sm overflow-hidden mx-5 whitespace-nowrap text-ellipsis flex">
               ssh root@{ip || "127.0.0.1"} ({OSData.OS})
             </p>
           </div>
+          
           <div className="flex-1 flex justify-end align-items-center pr-5">
-            {">"}
+            <button onClick={() => navigate(1)}>
+              <MdArrowForwardIos />
+            </button>
           </div>
         </div>
-        <div className="flex flex-col p-5 grow overflow-y-auto">
+        <div className="flex flex-col p-5 grow overflow-y-auto border-slate-700 border-2 rounded-b-lg">
           <Routes>
             <Route
               path="/"
