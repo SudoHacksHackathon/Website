5cec6ae9a55ef534ba9383bb1e54aff76774b4c8
Ayush Garg <ayushgargemail@gmail.com>
2022-07-21 21:27:23 -0700

    added tsParticles for background - need to fix interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -10,70 +10,89 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 import Draggable from "react-draggable";
 
+import Particles from "react-tsparticles";
+import type { Engine } from "tsparticles-engine";
+import { loadLinksPreset } from "tsparticles-preset-links";
+
 const App = () => {
+  const navigate = useNavigate();
+
   const ip = useIP();
   const OSData = getOSData();
 
-  const navigate = useNavigate();
+  const initParticles = async (engine: Engine) => {
+    await loadLinksPreset(engine);
+  };
 
+  // TODO: fix interactivity (it doesn't work??)
   return (
-    <div className="bg-[#111111] h-full w-full flex justify-center place-items-center selection:bg-slate-200 selection:text-black p-3">
-      <Draggable handle=".handle" bounds="parent">
-        <div className="rounded-xl w-[min(100%,_75rem)] h-[min(100%,_42.1875rem)] bg-black font-mono flex flex-col">
-          <div className="handle cursor-move rounded-t-xl w-full h-7 bg-white border-x-2 border-t-2 border-slate-700 select-none">
-            <div className="flex place-items-center w-full h-full">
-              <div className="flex justify-start align-items-center pl-3">
-                <button onClick={() => navigate(-1)}>
-                  <MdArrowBackIosNew />
-                </button>
-              </div>
+    <>
+      <Particles
+        id="tsparticles"
+        init={initParticles}
+        options={{
+          preset: "links",
+        }}
+        canvasClassName="blur-[2px] brightness-150"
+      />
+      <div className="bg-[#111111] h-full w-full flex justify-center place-items-center selection:bg-slate-200 selection:text-black p-3">
+        <Draggable handle=".handle" bounds="parent">
+          <div className="rounded-xl w-[min(100%,_75rem)] h-[min(100%,_42.1875rem)] bg-black font-mono flex flex-col">
+            <div className="handle cursor-move rounded-t-xl w-full h-7 bg-white border-x-2 border-t-2 border-slate-700 select-none">
+              <div className="flex place-items-center w-full h-full">
+                <div className="flex justify-start align-items-center pl-3">
+                  <button onClick={() => navigate(-1)}>
+                    <MdArrowBackIosNew />
+                  </button>
+                </div>
 
-              <div className="flex-1 flex justify-center align-items-center mx-3 w-0">
-                <p className="md:text-base text-sm overflow-hidden whitespace-nowrap text-ellipsis">
-                  {ip === undefined
-                    ? "Loading..."
-                    : `ssh root@${ip === null ? "127.0.0.1" : ip} (${
-                        OSData.OS
-                      })`}
-                </p>
-              </div>
+                <div className="flex-1 flex justify-center align-items-center mx-3 w-0">
+                  <p className="md:text-base text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+                    {ip === undefined
+                      ? "Loading..."
+                      : `ssh root@${ip === null ? "127.0.0.1" : ip} (${
+                          OSData.OS
+                        })`}
+                  </p>
+                </div>
 
-              <div className="flex justify-end align-items-center pr-3">
-                <button onClick={() => navigate(1)}>
-                  <MdArrowForwardIos />
-                </button>
+                <div className="flex justify-end align-items-center pr-3">
+                  <button onClick={() => navigate(1)}>
+                    <MdArrowForwardIos />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="flex flex-col p-5 grow overflow-y-auto border-slate-700 h-full border-2 border-t-0 rounded-b-lg">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <IntroScreen
-                    nextScreen={() => {
-                      navigate("/reserve");
-                    }}
-                  />
-                }
-              />
-              <Route
-                path="/reserve"
-                element={
-                  <SignupScreen
-                    nextScreen={() => {
-                      navigate("/submitted");
-                    }}
-                  />
-                }
-              />
-              <Route path="/submitted" element={<EndScreen />} />
-            </Routes>
+            <div className="flex flex-col p-5 grow overflow-y-auto border-slate-700 h-full border-2 border-t-0 rounded-b-lg">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <IntroScreen
+                      nextScreen={() => {
+                        navigate("/reserve");
+                      }}
+                    />
+                  }
+                />
+                <Route
+                  path="/reserve"
+                  element={
+                    <SignupScreen
+                      nextScreen={() => {
+                        navigate("/submitted");
+                      }}
+                    />
+                  }
+                />
+                <Route path="/submitted" element={<EndScreen />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Draggable>
-    </div>
+        </Draggable>
+      </div>
+    </>
   );
 };
 
